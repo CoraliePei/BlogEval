958479af76078818ad1d80e2f752971f5e5319db
Coralie Pei <pei.coralie@gmail.com>
2022-08-03 16:37:32 +0200

    Création des fonctions autoload pour fonctions et vendor

--- index.php
+++ index.php
@@ -4,6 +4,14 @@ spl_autoload_register(function ($className) {
     require_once './classes/' . $className . '.php';
 });
 
+require_once './functions/autoLoad.php';
+autoLoad("*.php");
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Définir le fuseau horaire dans lequel le serveur se trouve
+date_default_timezone_set('Europe/Paris');
+
 require_once './includes/header.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
