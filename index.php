bda4ff3a909d665e26bb9c2ba64f33a617c715ce
Coralie Pei <pei.coralie@gmail.com>
2022-08-03 16:01:35 +0200

    Autoload des classes

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+spl_autoload_register(function ($className) {
+    require_once './classes/' . $className . '.php';
+});
+
+
 require_once './includes/header.php';
+
+$connexionCategories = new Sql();
+$requeteCategoriesNiveau1 = "SELECT * FROM utilisateurs";
+$resultatCategories = $connexionCategories->select($requeteCategoriesNiveau1);
+var_dump($resultatCategories);
+
+
 require_once './includes/main.php';
 require_once './includes/footer.php';
