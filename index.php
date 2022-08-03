7327abe0a98976de28f584dd5b53ffe9099d080b
Coralie Pei <pei.coralie@gmail.com>
2022-08-03 16:22:05 +0200

    Fonction pour l'appel auto des pages

--- index.php
+++ index.php
@@ -4,14 +4,6 @@ spl_autoload_register(function ($className) {
     require_once './classes/' . $className . '.php';
 });
 
-
 require_once './includes/header.php';
-
-$connexionCategories = new Sql();
-$requeteCategoriesNiveau1 = "SELECT * FROM utilisateurs";
-$resultatCategories = $connexionCategories->select($requeteCategoriesNiveau1);
-var_dump($resultatCategories);
-
-
 require_once './includes/main.php';
 require_once './includes/footer.php';
