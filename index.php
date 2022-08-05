95ed3ec5bdd76fb57b884da3a04212d0fc875a64
Coralie Pei <pei.coralie@gmail.com>
2022-08-05 17:28:53 +0200

    Appel dans l'index du rôle admin pour changer le header

--- index.php
+++ index.php
@@ -13,6 +13,10 @@ require __DIR__ . '/vendor/autoload.php';
 // Définir le fuseau horaire dans lequel le serveur se trouve
 date_default_timezone_set('Europe/Paris');
 
-require_once './includes/header.php';
+if (verifierAdmin())
+    require_once './includes/headerAdmin.php';
+else
+    require_once './includes/header.php';
+
 require_once './includes/main.php';
 require_once './includes/footer.php';
