072ed5fc6c29008d1c5c718ea0a478f4798a18a1
Coralie Pei <pei.coralie@gmail.com>
2022-08-14 15:09:31 +0200

    Modification affichage de la liste article pour les visiteurs/users

--- includes/articles.inc.php
+++ includes/articles.inc.php
@@ -20,54 +20,96 @@ $menuCategories .= "</ul>";
 echo $menuCategories;
 
 
-if (!empty($_GET['id_categorie']) && ctype_digit($_GET['id_categorie'])) {
-    $id_categorie = $_GET['id_categorie'];
+// Partie affichage des articles publiés et brouillons pour les admins et rédacteurs
+if (verifierAdmin() || verifierRedacteur()) {
+    if (!empty($_GET['id_categorie']) && ctype_digit($_GET['id_categorie'])) {
+        $id_categorie = $_GET['id_categorie'];
 
-    $requeteArticlesCate = "SELECT * FROM articles INNER JOIN articles_has_categories ON articles.id_article = articles_has_categories.articles_id_article WHERE categories_id_categorie = $id_categorie, statut = 'publish' ORDER BY created_at DESC";
-    $connexionArticlesCate = new Sql();
-    $resultatArticles = $connexionArticlesCate->select($requeteArticlesCate);
+        $requeteArticlesCate = "SELECT * FROM articles INNER JOIN articles_has_categories ON articles.id_article = articles_has_categories.articles_id_article WHERE categories_id_categorie = $id_categorie ORDER BY created_at DESC";
+        $connexionArticlesCate = new Sql();
+        $resultatArticles = $connexionArticlesCate->select($requeteArticlesCate);
 
-    $menuArticlesCate = "<section class=\"wrap\">";
-    for ($i = 0; $i < count($resultatArticles); $i++) {
-        $menuArticlesCate .= "<div>";
-        $menuArticlesCate .= "<h2>";
-        $menuArticlesCate .= "<a href=\"index.php?page=articles&amp;id_categorie=" . $resultatArticles[$i]['categories_id_categorie'] . "&amp;id_article=" . $resultatArticles[$i]['id_article'] . "\">";
-        $menuArticlesCate .= $resultatArticles[$i]['titre'];
-        $menuArticlesCate .= "</a>";
-        $menuArticlesCate .= "</h2>";
-        $menuArticlesCate .= "<p>" . $resultatArticles[$i]['contenu'] . "</p>";
-        $menuArticlesCate .= "<p>Créé le : " . $resultatArticles[$i]['created_at'] . "</p>";
-        if (verifierAdmin() || verifierRedacteur()) {
+        $menuArticlesCate = "<section class=\"wrap\">";
+        for ($i = 0; $i < count($resultatArticles); $i++) {
+            $menuArticlesCate .= "<div>";
+            $menuArticlesCate .= "<h2>";
+            $menuArticlesCate .= "<a href=\"index.php?page=articles&amp;id_categorie=" . $resultatArticles[$i]['categories_id_categorie'] . "&amp;id_article=" . $resultatArticles[$i]['id_article'] . "\">";
+            $menuArticlesCate .= $resultatArticles[$i]['titre'];
+            $menuArticlesCate .= "</a>";
+            $menuArticlesCate .= "</h2>";
+            $menuArticlesCate .= "<p>" . $resultatArticles[$i]['contenu'] . "</p>";
+            $menuArticlesCate .= "<p>Créé le : " . $resultatArticles[$i]['created_at'] . "</p>";
             $menuArticlesCate .= "<a href=\"index.php?page=articlesEdit&amp;id_article=" . $resultatArticles[$i]['id_article'] . "\">Editer</a>";
             $menuArticlesCate .= " | ";
             $menuArticlesCate .= "<a href=\"index.php?page=articlesSuppr&amp;id_article=" . $resultatArticles[$i]['id_article'] . "\">Supprimer</a>";
+            $menuArticlesCate .= "</div>";
         }
-        $menuArticlesCate .= "</div>";
-    }
-    $menuArticlesCate .= "</section>";
-    echo $menuArticlesCate;
-} else {
-    $connexionArticles = new Sql();
-    $requeteArticles = $connexionArticles->select("SELECT * FROM articles");
+        $menuArticlesCate .= "</section>";
+        echo $menuArticlesCate;
+    } else {
+        $connexionArticles = new Sql();
+        $requeteArticles = $connexionArticles->select("SELECT * FROM articles");
 
-    $menuArticles = "<section class=\"wrap\">";
-    for ($i = 0; $i < count($requeteArticles); $i++) {
-        $menuArticles .= "<div>";
-        $menuArticles .= "<h2>";
-        $menuArticles .= "<a href=\"index.php?page=articles&amp;id_article=" . $requeteArticles[$i]['id_article'] . "\">";
-        $menuArticles .= $requeteArticles[$i]['titre'];
-        $menuArticles .= "</a>";
-        $menuArticles .= "</h2>";
-        $menuArticles .= "<p>" . $requeteArticles[$i]['contenu'] . "</p>";
-        $menuArticles .= "<p>Créé le : " . $requeteArticles[$i]['created_at'] . "</p>";
-        if (verifierAdmin() || verifierRedacteur()) {
+        $menuArticles = "<section class=\"wrap\">";
+        for ($i = 0; $i < count($requeteArticles); $i++) {
+            $menuArticles .= "<div>";
+            $menuArticles .= "<h2>";
+            $menuArticles .= "<a href=\"index.php?page=articles&amp;id_article=" . $requeteArticles[$i]['id_article'] . "\">";
+            $menuArticles .= $requeteArticles[$i]['titre'];
+            $menuArticles .= "</a>";
+            $menuArticles .= "</h2>";
+            $menuArticles .= "<p>" . $requeteArticles[$i]['contenu'] . "</p>";
+            $menuArticles .= "<p>Créé le : " . $requeteArticles[$i]['created_at'] . "</p>";
             $menuArticles .= "<a href=\"index.php?page=articlesEdit&amp;id_article=" . $requeteArticles[$i]['id_article'] . "\">Editer</a>";
             $menuArticles .= " | ";
             $menuArticles .= "<a href=\"index.php?page=articlesSuppr&amp;id_article=" . $requeteArticles[$i]['id_article'] . "\">Supprimer</a>";
+            $menuArticles .= "</div>";
         }
-        $menuArticles .= "</div>";
+        $menuArticles .= "</section>";
+        $menuArticles .= "</ul>";
+        echo $menuArticles;
     }
-    $menuArticles .= "</section>";
-    $menuArticles .= "</ul>";
-    echo $menuArticles;
+    // Partie affichage de la liste des articles publiés uniquement pour les visiteurs, utilisateurs et modérateurs
+} else {
+    if (!empty($_GET['id_categorie']) && ctype_digit($_GET['id_categorie'])) {
+        $id_categorie = $_GET['id_categorie'];
+
+        $requeteArticlesCate = "SELECT * FROM articles INNER JOIN articles_has_categories ON articles.id_article = articles_has_categories.articles_id_article WHERE categories_id_categorie = $id_categorie && statut = 'publish' ORDER BY created_at DESC";
+        $connexionArticlesCate = new Sql();
+        $resultatArticles = $connexionArticlesCate->select($requeteArticlesCate);
+
+        $menuArticlesCate = "<section class=\"wrap\">";
+        for ($i = 0; $i < count($resultatArticles); $i++) {
+            $menuArticlesCate .= "<div>";
+            $menuArticlesCate .= "<h2>";
+            $menuArticlesCate .= "<a href=\"index.php?page=articles&amp;id_categorie=" . $resultatArticles[$i]['categories_id_categorie'] . "&amp;id_article=" . $resultatArticles[$i]['id_article'] . "\">";
+            $menuArticlesCate .= $resultatArticles[$i]['titre'];
+            $menuArticlesCate .= "</a>";
+            $menuArticlesCate .= "</h2>";
+            $menuArticlesCate .= "<p>" . $resultatArticles[$i]['contenu'] . "</p>";
+            $menuArticlesCate .= "<p>Créé le : " . $resultatArticles[$i]['created_at'] . "</p>";
+            $menuArticlesCate .= "</div>";
+        }
+        $menuArticlesCate .= "</section>";
+        echo $menuArticlesCate;
+    } else {
+        $connexionArticles = new Sql();
+        $requeteArticles = $connexionArticles->select("SELECT * FROM articles WHERE statut = 'publish'");
+
+        $menuArticles = "<section class=\"wrap\">";
+        for ($i = 0; $i < count($requeteArticles); $i++) {
+            $menuArticles .= "<div>";
+            $menuArticles .= "<h2>";
+            $menuArticles .= "<a href=\"index.php?page=articles&amp;id_article=" . $requeteArticles[$i]['id_article'] . "\">";
+            $menuArticles .= $requeteArticles[$i]['titre'];
+            $menuArticles .= "</a>";
+            $menuArticles .= "</h2>";
+            $menuArticles .= "<p>" . $requeteArticles[$i]['contenu'] . "</p>";
+            $menuArticles .= "<p>Créé le : " . $requeteArticles[$i]['created_at'] . "</p>";
+            $menuArticles .= "</div>";
+        }
+        $menuArticles .= "</section>";
+        $menuArticles .= "</ul>";
+        echo $menuArticles;
+    }
 }
